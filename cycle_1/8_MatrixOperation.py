d12bf9c9e6276a87357f090198cd7ca3851eccf2
CoodeNoob <pg@PGs-MacBook-Air.local>
2026-08-13 10:40:26 +0530

    TFML APPLICATION

---
+++ cycle_1/8_MatrixOperation.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+matrix = [
+    [1,2,3],
+    [5,6,7]
+]
+
+matrix_1 = [
+    [1,2,3],
+    [5,6,7]
+]
+
+
+numpy_matrix= np.array(matrix)
+numpy_matrix_1= np.array(matrix_1)
+
+print(f"Sum of two matrix is \n{numpy_matrix+numpy_matrix_1}")
+print(f"Sub of two matrix is \n{numpy_matrix-numpy_matrix_1}")
+print(f"Multiply of two matrix is \n{numpy_matrix@numpy_matrix_1.T}")
