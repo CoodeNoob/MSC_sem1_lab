4e1a5eb3a41f226347f6e53f07332fafdb1bbef3
CoodeNoob <pg@PGs-MacBook-Air.local>
2026-08-13 11:32:30 +0530

    More Matrix Operations

--- cycle_1/8_MatrixOperation.py
+++ cycle_1/8_MatrixOperation.py
@@ -17,3 +17,7 @@ numpy_matrix_1= np.array(matrix_1)
 print(f"Sum of two matrix is \n{numpy_matrix+numpy_matrix_1}")
 print(f"Sub of two matrix is \n{numpy_matrix-numpy_matrix_1}")
 print(f"Multiply of two matrix is \n{numpy_matrix@numpy_matrix_1.T}")
+print(f"Transpose of the Matrix is {numpy_matrix_1.T}")
+
+# Trace is the sum of the main diagonal
+print(f"Trace of the Matrix is {np.trace(matrix)}")
